293f383577da0077f1999249f69e0d7afde21810
Alex Busch <abusch1013@gmail.com>
2020-02-26 19:36:48 -0700

    render some data in vue from our drupal API

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,17 +2,32 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul id="articles">
+      <li v-for="article in articles" :key="article.id">
+        {{ article.attributes.title }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-  import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue';
+  import axios from 'axios';
 
   export default {
     name: 'App',
     components: {
       HelloWorld
-    }
+    },
+    data() {
+      return {
+        articles: null
+      }
+    },
+    mounted() {
+      const res = axios.get('http://drupal.docker.localhost/api/articles');
+      res.then(res => (this.articles = res.data.data));
+    },
   }
 </script>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
-import axios from 'axios';
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-  mounted: () => {
-    const res = axios.get('http://drupal.docker.localhost/api/articles');
-    res.then(res => console.log(res.data.data));
-  },
 }).$mount('#app');
