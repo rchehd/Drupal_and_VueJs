90c8dfd8cdc7c01cb3cbe4a5a2afdca95c05bce6
Roman <roman.chekhaniuk@smile-ukraine.com>
2022-10-29 22:08:10 +0300

    Add header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <Test msg="Hello from test"></Test>
-    <ul id="articles">
-      <li v-for="article in articles" :key="article.id">
-        {{ article.attributes.title }}
-      </li>
-    </ul>
+    <div class="container">
+      <Header :image_url="photo"></Header>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+      <Test msg="Hello from test"></Test>
+      <ul id="articles">
+        <li v-for="article in articles" :key="article.id">
+          {{ article.attributes.title }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
   // import HelloWorld from './components/HelloWorld.vue';
+  import Header from "./components/Header";
   import Test from './components/Test.vue';
   import axios from 'axios';
-
+  import image from './assets/logo.png'
   export default {
     name: 'App',
+    image: image,
     components: {
+      Header,
       Test
     },
     data() {
@@ -30,6 +36,11 @@
       const res = axios.get('http://drupal.docker.localhost/api/articles');
       res.then(res => (this.articles = res.data.data));
     },
+    computed: {
+      photo() {
+        return require('./assets/logo.png');
+      }
+    }
   }
 </script>
 
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+      <img alt="Vue logo" :src="image_url" width="40" height="32" >
+      <span class="fs-4">Simple header</span>
+    </a>
+
+    <ul class="nav nav-pills">
+      <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+    </ul>
+  </header>
+</template>
+
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    image_url: String,
+    nav: Array
+  }
+}
+</script>
